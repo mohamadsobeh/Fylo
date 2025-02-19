cb42db269cdf0c6e55cc0114db849b827a88543d
mohamadsobeh <mohammadsobh1999@gmail.com>
2025-02-20 01:59:52 +0300

    Fixed image paths and updated styles

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,25 +1,28 @@
 import { useState } from "react";
 import FeatureBox from "./FeatureBox";
-
+import image1 from "../assets/images/icon-access-anywhere.svg"
+import image2 from "../assets/images/icon-security.svg"
+import image3 from "../assets/images/icon-collaboration.svg"
+import image4 from "../assets/images/icon-any-file.svg"
 function Features() {
   const [items, setItems] = useState([
     {
-      icon: "/src/assets/images/icon-access-anywhere.svg",
+      icon: image1,
       title: "Access your files, anywhere",
       desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut sed beatae, laboriosam enim voluptatum quia.",
     },
     {
-      icon: "/src/assets/images/icon-security.svg",
+      icon: image2,
       title: "Security you can trust",
       desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut sed beatae, laboriosam enim voluptatum quia.",
     },
     {
-      icon: "/src/assets/images/icon-collaboration.svg",
+      icon: image3,
       title: "Real-time collaboration",
       desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut sed beatae, laboriosam enim voluptatum quia.",
     },
     {
-      icon: "/src/assets/images/icon-any-file.svg",
+      icon: image4,
       title: "Store any type of file",
       desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut sed beatae, laboriosam enim voluptatum quia.",
     },
